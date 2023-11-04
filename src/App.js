ce7210a9086179bf87f7e25b4b96f795ebb661b0
ChetnaChandra123 <chetnachandra856@gmail.com>
2023-11-04 18:14:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import WordCounter from './WordCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <WordCounter />
+      
+    </div>
+  );
+}
+
+export default App;
